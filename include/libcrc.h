b5c3e7561c91a807a460165cf0cf3067c21a0384
Keeley Hoek <keeley@hoek.io>
2021-07-26 02:19:40 +1000

    Add Sensirion

--- include/libcrc.h
+++ include/libcrc.h
@@ -3,12 +3,11 @@
 
 #include <stddef.h>
 #include <stdint.h>
+#include <sys/cdefs.h>
 
-#define CRC8_LOOKUP_TABLE_SIZE 256
+#define CRC8_LOOKUP_TABLE_LEN 256
 
-// LaCrosse CRC8: poly=0x31, init=0x00, reflect_in=false, reflect_out=false, xor_out=0x00
-#define CRC8_LACROSSE_INIT 0x00
-extern const uint8_t CRC8_LACROSSE_LOOKUP_TABLE[CRC8_LOOKUP_TABLE_SIZE];
+extern const uint8_t CRC8_0x31_LOOKUP_TABLE[CRC8_LOOKUP_TABLE_LEN];
 
 static inline uint8_t crc8_calc_with_table(uint8_t init, const uint8_t *table, const uint8_t *buff, size_t len) {
     uint8_t crc = init;
@@ -18,8 +17,16 @@ static inline uint8_t crc8_calc_with_table(uint8_t init, const uint8_t *table, c
     return crc;
 }
 
-static inline uint8_t crc8_calc_lacrosse(const uint8_t *buff, size_t len) {
-    return crc8_calc_with_table(CRC8_LACROSSE_INIT, CRC8_LACROSSE_LOOKUP_TABLE, buff, len);
+// LaCrosse CRC8: poly=0x31, init=0x00, reflect_in=false, reflect_out=false, xor_out=0x00
+#define CRC8_LACROSSE_INIT 0x00
+static __always_inline uint8_t crc8_calc_lacrosse(const uint8_t *buff, size_t len) {
+    return crc8_calc_with_table(CRC8_LACROSSE_INIT, CRC8_0x31_LOOKUP_TABLE, buff, len);
+}
+
+// Sensirion CRC8: poly=0x31, init=0xFF, reflect_in=false, reflect_out=false, xor_out=0x00
+#define CRC8_SENSIRION_INIT 0xFF
+static __always_inline uint8_t crc8_calc_sensirion(const uint8_t *buff, size_t len) {
+    return crc8_calc_with_table(CRC8_SENSIRION_INIT, CRC8_0x31_LOOKUP_TABLE, buff, len);
 }
 
 #endif
